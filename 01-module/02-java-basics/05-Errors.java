2e0a60a92d4bcb92f79be496f32c7fcfc06762f8
kelevra-pro <damtat8@gmail.com>
2022-07-09 19:41:58 +0300

    resolve java-basics errors

---
+++ 01-module/02-java-basics/05-Errors.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("What Is Dead May Never Die!");
+        // END
+    }
+}
