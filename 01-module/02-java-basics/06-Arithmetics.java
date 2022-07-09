583358d2ddff2a69d1e18a5dc1c47a4a8413c16e
kelevra-pro <damtat8@gmail.com>
2022-07-09 19:47:49 +0300

    resolve java-basics arithmetics

--- 01-module/02-java-basics/06-Arithmetics.java
+++ 01-module/02-java-basics/06-Arithmetics.java
@@ -1,5 +1,5 @@
 class App {
-    public static void printResult(String[] args) {
+    public static void main(String[] args) {
         // BEGIN (write your solution here)
         System.out.println(-8 / -4);
         System.out.println(100 % 3);
