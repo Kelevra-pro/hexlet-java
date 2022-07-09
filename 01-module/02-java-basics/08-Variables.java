476803b76923d8fb5d4ebb35f24dfaf5879cb278
kelevra-pro <damtat8@gmail.com>
2022-07-09 23:11:15 +0300

    resolve java-basics variables

---
+++ 01-module/02-java-basics/08-Variables.java
@@ -0,0 +1,14 @@
+class App {
+    public static void main(String[] args) {
+        var family = "Targaryen";
+
+        // BEGIN (write your solution here)
+        var pet = "Dragon";
+        // END
+
+        System.out.print(family);
+        System.out.print(" and ");
+        System.out.print(pet);
+    }
+
+}
