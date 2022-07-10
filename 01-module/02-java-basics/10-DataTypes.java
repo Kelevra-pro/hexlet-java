5846d2b74e122fba16aa917b127d158f79bd644b
kelevra-pro <damtat8@gmail.com>
2022-07-10 21:15:46 +0300

    resolve java-basics data-types

---
+++ 01-module/02-java-basics/10-DataTypes.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println((int) 2.9 + " times");
+        // END
+    }
+}
