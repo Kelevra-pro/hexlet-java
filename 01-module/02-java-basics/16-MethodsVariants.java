3cab05337bbe7532d93a5e7b47fedcd1fdd7dd1e
kelevra-pro <damtat8@gmail.com>
2022-07-16 23:07:53 +0300

    resolve java-basics methods-variants

---
+++ 01-module/02-java-basics/16-MethodsVariants.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        var emoji = "-(";
+        // BEGIN (write your solution here)
+        var happyEmoji = ":" + emoji.replace('(', ')');
+        System.out.println(happyEmoji);
+        // END
+    }
+}
