c9d4014ece6b03bf6ab1b0f9a5ae6b0c347ae280
kelevra-pro <damtat8@gmail.com>
2022-07-16 22:23:49 +0300

    resolve java-basics pure-functions

---
+++ 01-module/02-java-basics/14-PureFunctions.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println((int) Math.random() * 10);
+        // END
+    }
+}
