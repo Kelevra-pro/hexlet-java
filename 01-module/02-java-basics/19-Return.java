ca521744e20d7df434b0bd06b3a4d3fc02d9ff56
kelevra-pro <damtat8@gmail.com>
2022-07-18 23:36:16 +0300

    resolve java-basics return

---
+++ 01-module/02-java-basics/19-Return.java
@@ -0,0 +1,12 @@
+public class App {
+    // BEGIN (write your solution here)
+    public static String sayHurrayThreeTimes() {
+        var word = "hurray!";
+        return word + " " + word + " " + word;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(App.sayHurrayThreeTimes());
+    }
+    // END
+}
