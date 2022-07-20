aa5b679e3fa09344c4e450a185d396ca1dc81a38
kelevra-pro <damtat8@gmail.com>
2022-07-20 22:22:25 +0300

    resolve java-basics logical-operations

---
+++ 01-module/02-java-basics/22-LogicalOperations.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    // BEGIN (write your solution here)
+    public static boolean isPalindrome(String str) {
+        return StringUtils.reverse(str).equalsIgnoreCase(str);
+    }
+    // END
+}
